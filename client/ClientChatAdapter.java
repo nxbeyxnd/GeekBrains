0ebac8963be94d2af20f7c029cac5c7d6b3ad9d1
nxbeyxnd <unblocksteam@gmail.com>
2020-12-17 23:57:12 +0300

    lvl2 les8

---
+++ client/ClientChatAdapter.java
@@ -0,0 +1,39 @@
+package client;
+
+import gui.ChatFrame;
+
+import java.util.function.Consumer;
+
+public class ClientChatAdapter {
+    private ChatFrame chatFrame;
+    private Client client;
+
+    public ClientChatAdapter(String host, int port) {
+        client = new Client(host, port);
+        chatFrame = new ChatFrame(new Consumer<String>() {
+            @Override
+            public void accept(String messageFromFormSubmitListener) {
+                client.sendMessage(messageFromFormSubmitListener);
+            }
+        });
+        read();
+    }
+
+    private void read() {
+        new Thread(() -> {
+            try {
+                while (true) {
+                    chatFrame.append(
+                            client.receiveMessage()
+                    );
+                }
+            } catch (ClientConnectionException e) {
+                throw e;
+            } finally {
+                if (client != null) {
+                    client.close();
+                }
+            }
+        }).start();
+    }
+}
---
+++ server/AuthenticationService.java
@@ -0,0 +1,49 @@
+package server;
+
+import java.util.Set;
+
+public class AuthenticationService {
+    private Set<CredentialsEntry> entries;
+
+    public AuthenticationService() {
+        entries = Set.of(
+                new CredentialsEntry("l1", "p1", "nickname1"),
+                new CredentialsEntry("l2", "p2", "nickname2"),
+                new CredentialsEntry("l3", "p3", "nickname3")
+        );
+    }
+
+    public String findNicknameByLoginAndPassword(String login, String password) {
+        for (CredentialsEntry entry : entries) {
+            if (entry.getLogin().equals(login) && entry.getPassword().equals(password)) {
+                return entry.getNickname();
+            }
+        }
+        return null;
+    }
+
+    public static class CredentialsEntry {
+        private String login;
+        private String password;
+        private String nickname;
+
+        public CredentialsEntry(String login, String password, String nickname) {
+            this.login = login;
+            this.password = password;
+            this.nickname = nickname;
+        }
+
+
+        public String getLogin() {
+            return login;
+        }
+
+        public String getPassword() {
+            return password;
+        }
+
+        public String getNickname() {
+            return nickname;
+        }
+    }
+}
