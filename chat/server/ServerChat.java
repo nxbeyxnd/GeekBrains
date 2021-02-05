43cd3adf475da93991888df81804bb9b73c8f601
nxbeyxnd <unblocksteam@gmail.com>
2021-02-05 22:48:07 +0300

    Added Chat Logger

---
+++ chat/server/ServerChat.java
@@ -0,0 +1,74 @@
+package server;
+
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.util.Date;
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
+import java.util.logging.Logger;
+
+public class ServerChat implements Chat {
+    private static final Logger logger = Logger.getLogger(String.valueOf(ServerChat.class));
+    private ServerSocket serverSocket;
+    private Set<ClientHandler> clients;
+    private AuthenticationService authenticationService;
+
+    public ServerChat() {
+        start();
+    }
+
+    @Override
+    public AuthenticationService getAuthenticationService() {
+        return authenticationService;
+    }
+
+    private void start() {
+        try {
+            serverSocket = new ServerSocket(8888);
+            clients = new HashSet<>();
+            authenticationService = new AuthenticationService();
+            logger.info("Server was started!");
+
+            //System.out.println(ConnectionService.connect());
+
+            while (true) {
+                System.out.println("Server is waiting for a connection ...");
+                Socket socket = serverSocket.accept();
+                ClientHandler clientHandler = new ClientHandler(socket, this);
+                System.out.println(String.format("[%s] Client[%s] is successfully logged in", new Date(), clientHandler.getName()));
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public synchronized void broadcastMessage(String message) {
+        for (ClientHandler client : clients) {
+            client.sendMessage(message);
+        }
+    }
+
+    @Override
+    public synchronized boolean isNicknameOccupied(String nickname) {
+        for (ClientHandler client : clients) {
+            if (client.getName().equals(nickname)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public synchronized void subscribe(ClientHandler client) {
+        clients.add(client);
+    }
+
+    @Override
+    public synchronized void unsubscribe(ClientHandler client) {
+        clients.remove(client);
+    }
+}
